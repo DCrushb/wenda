665f1bec65d35379616819800d0184e992414fbf
Dacheng <wwee4411@sina.com>
2020-07-01 23:21:50 +0800

    wemda first commit

---
+++ src/main/java/com/nowcoder/async/EventProducer.java
@@ -0,0 +1,33 @@
+package com.nowcoder.async;
+
+import com.alibaba.fastjson.JSONObject;
+import com.nowcoder.util.JedisAdapter;
+import com.nowcoder.util.RedisKeyUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * Created by nowcoder on 2016/7/30.
+ */
+@Service
+public class EventProducer {
+    @Autowired
+    JedisAdapter jedisAdapter;
+
+    public boolean fireEvent(EventModel eventModel) {
+        try {
+            Object o = new Object();
+//            o.equals()；
+            String json = JSONObject.toJSONString(eventModel);
+            String key = RedisKeyUtil.getEventQueueKey();
+            jedisAdapter.lpush(key, json);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
